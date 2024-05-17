ad2aa2a908f21ade954021a3fe995408c864d54f
Leon <leon@bencom.nl>
2024-05-17 15:02:45 +0200

    setup expo-font

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Danfo: require('./assets/Danfo-Regular.ttf'),
+  });
+
+  console.log({ fontError })
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text>{JSON.stringify({ fontsLoaded, fontError }, null, 2)}</Text>
+        <Text style={{ fontFamily: "Danfo" }}>Danfo</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +26,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
